5817fcefc8380ccef3bc0c2d16fb76bb87dab513
KhaledMAlasmari <sirkhaled7@gmail.com>
2021-08-29 09:48:06 +0300

    first version

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ import chatsRouter from './controllers/chats/chatsRouter'
 const app = express()
 app.use(cors())
 app.use(express.json())
-
+app.use(express.static('public'))
 app.use(tokenExtractor)
 app.use(userExtractor)
 
